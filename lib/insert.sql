717fbbdda478914b36f0244ea40d648f6d79b147
Jazper <jazpersaldana@yahoo.com>
2024-06-18 14:50:09 -0700

    organzing bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 6, 'M', 'Brown', 'Sexy', 1),
+('Rowdy', 10, 'M', 'Black', 'Rowdy', 0),
+('Tabitha', 9, 'F', 'Yellow', 'Subdued', 1),
+('Sergeant Brown', 8, 'M', 'Black', 'Hardcore', 1),
+('Melissa', 11, 'F', 'White', 'Chillin', 0),
+('Grinch', 6, 'M', 'Green', 'Mean', 0),
+('Wendy', 4, 'F', 'Orange', 'Mysterious', 1),
+(NULL, NULL, NULL, 'Unknown', 'Unknown', 1)
